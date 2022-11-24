1ce85b5cbe5e099b4132c70483eef4eb0df88882
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2022-11-24 15:39:56 +0300

    fix linter errors

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
-function show_menu() {
-  var popup_menu = document.getElementById('popup_menu')
-  popup_menu.style.display = 'block'
+function showMenu() {
+  const popupMenu = document.getElementById('popup_menu');
+  popupMenu.style.display = 'block';
 }
 
-function close_menu() {
-  var popup_menu = document.getElementById('popup_menu')
-  popup_menu.style.display = 'none'
+function closeMenu() {
+  const popupMenu = document.getElementById('popup_menu');
+  popupMenu.style.display = 'none';
 }
 
-function create_feature(vehicle) {
+function createFeature(vehicle) {
   const feature = `
     <feature class="feature">
       <img src="${vehicle.image}" alt="featured_image">
@@ -21,24 +21,72 @@ function create_feature(vehicle) {
           <li>Engine: ${vehicle.engine} </li>
           <li>hp: ${vehicle.horsepower} </li>
         </ul>
-        <a onclick="show_feature_popup(${vehicle.id})" href="#featured_content">More details</a>
+        <a onclick="showFeaturePopup(${vehicle.id})" href="#featured_content">More details</a>
       </info>
-    </feature>`
+    </feature>`;
   const parent = document.getElementById('featured_content');
   parent.innerHTML += feature;
 }
 
-function load_feature() {
-  const vehicles = list_vehicles()
-  for (let x = 0; x < vehicles.length; x++) {
-    create_feature(vehicles[x])
+function listVehicles() {
+  const vehicle1 = {
+    id: 1,
+    name: 'AudiR8',
+    year: 2022,
+    engine: '5.2L V10',
+    horsepower: '562',
+    transmission: 'manual',
+    image: 'images/about_page/featured/audiR8_featured.png',
+    dealer: 'Extreme auto dealers',
+    country: 'Kenya',
+  };
+  const vehicle2 = {
+    id: 2,
+    name: 'Ford Mustang Roush',
+    year: 2018,
+    engine: '5.0L V8',
+    horsepower: '710',
+    transmission: 'manual',
+    image: 'images/about_page/featured/ford-mustang-roush.png',
+    dealer: 'Cranes auto',
+    country: 'Uganda',
+  };
+  const vehicle3 = {
+    id: 3,
+    name: 'Rolls-Royce Phantom',
+    year: 2003,
+    engine: '6.75L V12',
+    horsepower: '453',
+    transmission: 'automatic',
+    image: 'images/about_page/featured/rolls-royce-phanton.png',
+    dealer: 'Kigali auto dealers',
+    country: 'Rwanda',
+  };
+  const vehicle4 = {
+    id: 4,
+    name: 'Bentley Continental',
+    year: 2022,
+    engine: '4.0L V8',
+    horsepower: '542',
+    transmission: 'manual',
+    image: 'images/about_page/featured/bentley_continental.png',
+    dealer: 'Habesha Automotives',
+    country: 'Ethiopia',
+  };
+  return [vehicle1, vehicle2, vehicle3, vehicle4];
+}
+
+function loadFeature() {
+  const vehicles = listVehicles();
+  for (let x = 0; x < vehicles.length; x += 1) {
+    createFeature(vehicles[x]);
   }
 }
 
-function create_feature_popup(vehicle) {
+function createFeaturePopup(vehicle) {
   const popup = `
     <popup-feature id="feature_popup">
-      <a onclick="close_feature_popup()" id="close_feature_popup" href="#featured_content"><img src="images/popup/close_window.png" alt="close"></a>
+      <a onclick="closeFeaturePopup()" id="close_feature_popup" href="#featured_content"><img src="images/popup/close_window.png" alt="close"></a>
       <div class="feature">
         <img src="${vehicle.image}" alt="featured_image">
         <info class="feature_info">
@@ -55,69 +103,28 @@ function create_feature_popup(vehicle) {
           </ul>
         </info>
       </div>
-    </popup-feature>`
+    </popup-feature>`;
   const parent = document.getElementById('featured_content');
   parent.innerHTML += popup;
 }
 
-function show_feature_popup(vehicleId) {
-  const vehicles = list_vehicles();
+function showFeaturePopup(vehicleId) {
+  const vehicles = listVehicles();
   const vehicleIndex = vehicles.findIndex((object) => object.id === vehicleId);
   const vehicle = vehicles[vehicleIndex];
-  create_feature_popup(vehicle)
-  var feature_popup = document.getElementById('feature_popup')
-  feature_popup.style.display = 'block'
+  createFeaturePopup(vehicle);
+  const featurePopup = document.getElementById('feature_popup');
+  featurePopup.style.display = 'block';
 }
 
-function close_feature_popup() {
-  var feature_popup = document.getElementById('feature_popup')
-  feature_popup.style.display = 'none'
+function closeFeaturePopup() {
+  const featurePopup = document.getElementById('feature_popup');
+  featurePopup.style.display = 'none';
 }
 
-function list_vehicles() {
-  const vehicle1 = {
-    id: 1,
-    name: 'AudiR8',
-    year: 2022,
-    engine: '5.2L V10',
-    horsepower: '562',
-    transmission: 'manual',
-    image: 'images/about_page/featured/audiR8_featured.png',
-    dealer: 'Extreme auto dealers',
-    country: 'Kenya'
-  }
-  const vehicle2 = {
-    id: 2,
-    name: 'Ford Mustang Roush',
-    year: 2018,
-    engine: '5.0L V8',
-    horsepower: '710',
-    transmission: 'manual',
-    image: 'images/about_page/featured/ford-mustang-roush.png',
-    dealer: 'Cranes auto',
-    country: 'Uganda'
-  }
-  const vehicle3 = {
-    id: 3,
-    name: 'Rolls-Royce Phantom',
-    year: 2003,
-    engine: '6.75L V12',
-    horsepower: '453',
-    transmission: 'automatic',
-    image: 'images/about_page/featured/rolls-royce-phanton.png',
-    dealer: 'Kigali auto dealers',
-    country: 'Rwanda'
-  }
-  const vehicle4 = {
-    id: 4,
-    name: 'Bentley Continental',
-    year: 2022,
-    engine: '4.0L V8',
-    horsepower: '542',
-    transmission: 'manual',
-    image: 'images/about_page/featured/bentley_continental.png',
-    dealer: 'Habesha Automotives',
-    country: 'Ethiopia'
-  }
-  return [vehicle1, vehicle2, vehicle3, vehicle4]
-}
+showMenu();
+closeMenu();
+loadFeature();
+createFeaturePopup();
+showFeaturePopup();
+closeFeaturePopup();
